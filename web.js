5f2614a41e7f9c5af7b1088b0a62871c3c0fdf71
dartisimus <dartisimus@gmail.com>
2013-07-12 18:19:44 +0000

    Changed

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
-
+var fs = require("fs");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- var buffer = new Buffer(fs.readFileSync(index.html))
- response.send(buffer.toString());
+ var fs = require("fs");
+ var buffer = fs.readFileSync(index.html);
+ response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
